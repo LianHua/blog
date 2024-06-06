a7318c64e3b93125724bc3125076d2a321772b14
LianHua <LianHua@users.noreply.github.com>
2024-06-06 05:31:18 +0000

    deploy: LianHua/hugo@5153bfbdc315441c86620c8dd7a74cf8be32c0f5

---
+++ 友链/friends.js
@@ -0,0 +1,24 @@
+function loadQexoFriends(id, url) {
+    var uri = url + "/pub/friends/";
+    var loadStyle = '<center><div class="qexo_loading"><div class="qexo_part"><div style="display: flex; justify-content: center"><div class="qexo_loader"><div class="qexo_inner one"></div><div class="qexo_inner two"></div><div class="qexo_inner three"></div></div></div></div><p style="text-align: center; display: block">友链加载中...</p></div><center>';
+    document.getElementById(id).className = "friend-content";
+    document.getElementById(id).innerHTML = loadStyle;
+    fetch(uri).then(function(data){
+        if (data.ok){
+            data.json().then(function(res){
+            if (res["status"]) {
+                    var friends = res["data"];
+                    let content = "";
+                    for (let i = 0; i < friends.length; i++) {
+                        content += '<p><a target="_blank" href="' + friends[i]["url"] + '" title="' + friends[i]["name"] + '" class="qexo-friendurl"></p><div class="qexo-frienddiv"><div class="qexo-frienddivleft"><img class="qexo-myfriend" src="' + friends[i]["image"] + '"></div><div class="qexo-frienddivright"><span style="font-weight: bold;">' + friends[i]["name"] + '</span><br>' + friends[i]["description"] + '</div></div></a>';
+                    }
+                    document.getElementById(id).innerHTML = content + "";
+                } else {
+                    alert("友链载入失败!");
+                }
+            });
+        } else {
+            alert("友链获取失败!");
+        }
+    });
+}
